be390b3947631a0aae091e1cb774976e30a5e7e1
Mustafa Al Ogaidi <mustafaalogaidi@Mustafas-MacBook-Air.local>
2021-05-17 18:48:41 -0500

    alignment createinstance.tf

--- Start With Terraform Basics/createinstance.tf
+++ Start With Terraform Basics/createinstance.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-    access_key = "AKIAQOMAYMC27Y5YFGCD"
-    secret_key = "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"
-    region = "us-east-1"
+    access_key  = "AKIAQOMAYMC27Y5YFGCD"
+    secret_key  = "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"
+    region      = "us-east-1"
 }
 
 resource "aws-instance" "MyFirstInstance" {
-    ami = "ami-0ee02acd56a52998e"
-    instance-type = "t2.micro"  
+    ami             = "ami-0ee02acd56a52998e"
+    instance-type   = "t2.micro"  
 }
