4484337564a3bca47c240284c08868fbd936f361
Mustafa Al Ogaidi <mustafaalogaidi@Mustafas-MacBook-Air.local>
2021-05-18 18:25:28 -0500

    clean up provider.tf

--- Start With Terraform Basics/provider.tf
+++ Start With Terraform Basics/provider.tf
@@ -10,6 +10,6 @@ terraform {
 
 provider "aws" {
     region      = "us-east-1"
-    access_key  = "AKIAQOMAYMC27Y5YFGCD"
-    secret_key  = "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"
+    access_key  = "AKIAQOMAYMC2RKBXH2AO"
+    secret_key  = "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"
 }
