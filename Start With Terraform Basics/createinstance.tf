73d8d72cf4f4d97db1f7b22d317831a651dea128
Mustafa Al Ogaidi <mustafaalogaidi@Mustafas-Air.attlocal.net>
2021-12-23 14:14:14 -0600

    clean up folder

--- Start With Terraform Basics/createinstance.tf
+++ Start With Terraform Basics/createinstance.tf
@@ -1,4 +1,9 @@
 resource "aws_instance" "MyFirstInstance" {
+    count = 3
     ami             = "ami-0d563aeddd4be7fff"
     instance_type   = "t2.micro"  
+    
+    tags = {
+      Name = "demoinstance-${count.index}"
+    }
 }
